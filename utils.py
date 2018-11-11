716621bb7c4a5690ec736398b35e128bc17c210c
Sophie <zouyining@live.cn>
2018-11-11 18:48:39 -0500

    load the chord annotation to ndarr. and transpose them

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 import numpy as np
 from scipy.io import wavfile
 import samplerate
+import csv
+
+
 """
 Note: to install samplerate, run
 $ pip install samplerate
@@ -71,8 +74,156 @@ class DataReader:
 
 
 class ChordReader:
-   raise NotImplementedError
+    def __init__(self, encode_length):
+        '''
+        Initialize the ChordReader Object
+        :param encode_length: length of the one-hot encoding of chord, 24 for only major/min chords, 40 for all chords
+        '''
+        self.beat_path = './The Beatles Annotations/beat/'
+        self.chord_path = './The Beatles Annotations/chord/'
+        self.encode_length = encode_length
 
+    def __call__(self, album, song, pitch=0):
+        '''
+        :param album: <'01', '02', '03', '06', '07', '12'>
+        :param song: <'01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14'>
+        Convert the chord annotation into a ndarray
+        return each element in the array is a 24-bit vector, represent the chord of the this beat
+        '''
+        beat_file = '{}{}/{}.txt'.format(self.beat_path, album, song)
+        chord_file = '{}{}/{}.lab'.format(self.chord_path, album, song)
+        
+        # read beat file and convert it to ndarray 
+        beat = np.loadtxt(beat_file, delimiter=' ')
 
-class ChordPitchShifter:
-    raise NotImplementedError
+        chord = [] # each element in the array are in this form: [start time, end time, chord, major/minor: 1/0]
+        tmp_array = []
+
+        # Read the chord file and mordify the chord annotations to only include major and minor chords
+        with open(chord_file) as data:
+            reader = csv.reader(data, delimiter = ' ')
+            for row in reader:
+                tmp_array.append(row)
+
+        for i in range(len(tmp_array)):
+            if ':' not in tmp_array[i][2]:
+                chord[i] = [tmp_array[i][0], tmp_array[i][1], tmp_array[i][2], 1]
+            else:
+                elem = tmp_array[i].split(':')
+                root = elem[0]
+                short_hand = elem[1]
+                if '(' in short_hand:
+                    short_hand = short_hand.split('(')[0]
+                if '/' in short_hand:
+                    short_hand = short_hand.split('/')[0]
+                
+                if (short_hand == 'min') or (short_hand == 'dim') or (short_hand == 'min7') or (short_hand == 'dim7') or (short_hand == 'hdim7') or (short_hand == 'minmaj7') or (short_hand == 'min6') or (short_hand == 'min9'):
+                    chord[i] = [tmp_array[i][0], tmp_array[i][1], root, 0]
+                elif (short_hand == 'aug') or (short_hand == 'maj7') or (short_hand == '7') or (short_hand == 'maj6') or (short_hand == 'maj9') or (short_hand == '9') or (short_hand == 'sus4'):
+                    chord[i] = [tmp_array[i][0], tmp_array[i][1], root, 1]
+                else:
+                    chord[i] = [tmp_array[i][0], tmp_array[i][1], root, -1]
+        
+        # construct the chord_vector where each element of this vector is a one-hot code representing the chord of the beat
+        num_beat = np.shape(beat)[0]
+        chord_vector = np.zeros([num_beat, encode_length])
+
+        a = 0
+        tmp = chord[a][1]
+        for i in range(num_beat):
+            if beat[i][0] <= tmp:
+                if chord[i][2] == "C":
+                    if chord[i][3] == 1:
+                        chord_vector[i][0] = 1
+                    elif chord[i][3] == 0:
+                        chord_vector[i][1] = 1
+                    else:
+                        chord_vector[i] = 0
+                elif chord[i][2] == "C#":
+                    if chord[i][3] == 1:
+                        chord_vector[i][2] = 1
+                    elif chord[i][3] == 0:
+                        chord_vector[i][3] = 1
+                    else:
+                        chord_vector[i] = 0
+                elif chord[i][2] == "D":
+                    if chord[i][3] == 1:
+                        chord_vector[i][4] = 1
+                    elif chord[i][3] == 0:
+                        chord_vector[i][5] = 1
+                    else:
+                        chord_vector[i] = 0
+                elif chord[i][2] == "D#":
+                    if chord[i][3] == 1:
+                        chord_vector[i][6] = 1
+                    elif chord[i][3] == 0:
+                        chord_vector[i][7] = 1
+                    else:
+                        chord_vector[i] = 0
+                elif chord[i][2] == "E":
+                    if chord[i][3] == 1:
+                        chord_vector[i][8] = 1
+                    elif chord[i][3] == 0:
+                        chord_vector[i][9] = 1
+                    else:
+                        chord_vector[i] = 0
+                elif chord[i][2] == "F":
+                    if chord[i][3] == 1:
+                        chord_vector[i][10] = 1
+                    elif chord[i][3] == 0:
+                        chord_vector[i][11] = 1
+                    else:
+                        chord_vector[i] = 0
+                elif chord[i][2] == "F#":
+                    if chord[i][3] == 1:
+                        chord_vector[i][12] = 1
+                    elif chord[i][3] == 0:
+                        chord_vector[i][13] = 1
+                    else:
+                        chord_vector[i] = 0
+                elif chord[i][2] == "G":
+                    if chord[i][3] == 1:
+                        chord_vector[i][14] = 1
+                    elif chord[i][3] == 0:
+                        chord_vector[i][15] = 1
+                    else:
+                        chord_vector[i] = 0
+                elif chord[i][2] == "G#":
+                    if chord[i][3] == 1:
+                        chord_vector[i][16] = 1
+                    elif chord[i][3] == 0:
+                        chord_vector[i][17] = 1
+                    else:
+                        chord_vector[i] = 0
+                elif chord[i][2] == "A":
+                    if chord[i][3] == 1:
+                        chord_vector[i][18] = 1
+                    elif chord[i][3] == 0:
+                        chord_vector[i][19] = 1
+                    else:
+                        chord_vector[i] = 0
+                elif chord[i][2] == "A#":
+                    if chord[i][3] == 1:
+                        chord_vector[i][20] = 1
+                    elif chord[i][3] == 0:
+                        chord_vector[i][21] = 1
+                    else:
+                        chord_vector[i] = 0
+                elif chord[i][2] == "B":
+                    if chord[i][3] == 1:
+                        chord_vector[i][22] = 1
+                    elif chord[i][3] == 0:
+                        chord_vector[i][23] = 1
+                    else:
+                        chord_vector[i] = 0
+            else:
+                while beat[i][0] > tmp:
+                    a += 1
+                    tmp = chord[a][1]
+
+        # pitch shift the labels of the chord 
+        chord_vector_shifted = np.zeros([num_beat, encode_length])
+        for j in range(num_beat):
+            chord_vector_shifted[j]= chord_vector[j][pitch, :] + chord_vector[j][:, pitch]
+            
+        return chord_vector_shifted
