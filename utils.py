2f742572b43d244dbcc3401cacf8353e19937516
John Zhou <johnyunfanzhou@gmail.com>
2018-11-19 16:12:46 -0500

    Modified ChordReader to correct No_Chord label loading

--- utils.py
+++ utils.py
@@ -75,140 +75,173 @@ class DataReader:
 
 
 class ChordReader:
-    def __init__(self, encode_length):
-        '''
+    def __init__(self, mode='simple'):
+        """
         Initialize the ChordReader Object
-        :param encode_length: length of the one-hot encoding of chord, 24 for only major/min chords, 40 for all chords
-        '''
+        :param mode: 'simple' for recognizing only major and minor, 'advanced' for more detailed recognition
+
+        """
         self.beat_path = './The Beatles Annotations/beat/'
         self.chord_path = './The Beatles Annotations/chord/'
-        self.encode_length = encode_length
+
+        self.root_list = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+        self.sh_list = ['maj', 'min', 'dim', 'aug', 'maj7', 'min7', '7', 'dim7', 'hdim7', 'minmaj7', 'maj6', 'min6', '9', 'maj9', 'min9', 'sus4']
+
+        if mode == 'simple':
+            self.encode_length = 25
+        elif mode == 'advanced':
+            self.encode_length = 41
+        else:
+            raise ValueError('Invalid ChordReader mode. Should be either \'simple\' or \'advanced\'')
 
     def __call__(self, album, song, pitch=0):
-        '''
+        """
         :param album: <'01', '02', '03', '06', '07', '12'>
         :param song: <'01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14'>
         Convert the chord annotation into a ndarray
-        return each element in the array is a 24-bit vector, represent the chord of the this beat
-        '''
+
+        :return: ndarray of shape [num_beats - 1, encode_length]
+
+        """
         beat_file = '{}{}/{}.txt'.format(self.beat_path, album, song)
         chord_file = '{}{}/{}.lab'.format(self.chord_path, album, song)
 
         pitch = pitch % 12
 
-        # read beat file and convert it to ndarray 
+        # read beat timings (from the beat file) and convert it to ndarray
         beat = np.loadtxt(beat_file, delimiter=' ')[:, 0]
 
-        chord = [] # each element in the array are in this form: [start time, end time, chord, major/minor: 1/0]
-        tmp_array = []
+        chord = []  # each element in the array are in this form: [start time, end time, root (or 'N'), short_hand (or 'N')]
 
-        # Read the chord file and mordify the chord annotations to only include major and minor chords
-        with open(chord_file) as data:
-            reader = csv.reader(data, delimiter=' ')
+        # Read the chord file and modify the chord annotations to only include major and minor chords
+        raw_array = []
+        with open(chord_file) as labfile:
+            reader = csv.reader(labfile, delimiter=' ')
             for row in reader:
-                tmp_array.append(row)
+                raw_array.append(row)
 
-        for i in range(len(tmp_array)):
-            chord.append([0, 0, 0, 0])
-
-            elem = tmp_array[i][2]
-            if '(' in elem:
-                elem = elem.split('(')[0]
-            if '/' in elem:
-                elem = elem.split('/')[0]
-
-            if ':' not in elem:
-                chord[i] = [tmp_array[i][0], tmp_array[i][1], elem, 1]
-            else:
-                pair = elem.split(':')
-                root = pair[0]
-                short_hand = pair[1]
-                
-                if (short_hand == 'min') or (short_hand == 'dim') or (short_hand == 'min7') or (short_hand == 'dim7') or (short_hand == 'hdim7') or (short_hand == 'minmaj7') or (short_hand == 'min6') or (short_hand == 'min9'):
-                    chord[i] = [tmp_array[i][0], tmp_array[i][1], root, 0]
-                elif (short_hand == 'aug') or (short_hand == 'maj7') or (short_hand == '7') or (short_hand == 'maj6') or (short_hand == 'maj9') or (short_hand == '9') or (short_hand == 'sus4'):
-                    chord[i] = [tmp_array[i][0], tmp_array[i][1], root, 1]
+        # load the file to the chord array
+        if self.encode_length == 25:
+            for i in range(len(raw_array)):
+                elem = raw_array[i][2]
+                if '(' in elem:
+                    elem = elem.split('(')[0]
+                if '/' in elem:
+                    elem = elem.split('/')[0]
+                if ':' in elem:
+                    elem = elem.split(':')
+                    root = self.root_list.index(elem[0])
+                    if elem[1] == '':
+                        short_hand = 0
+                    else:
+                        short_hand = self.sh_list.index(elem[1])
                 else:
-                    chord[i] = [tmp_array[i][0], tmp_array[i][1], root, -1]
-        
-        # construct the chord_vector where each element of this vector is a one-hot code representing the chord of the beat
-        num_beat = np.shape(beat)[0]
-        chord_vector = np.zeros([num_beat - 1, self.encode_length])
+                    if elem == 'N':
+                        root = -1
+                        short_hand = -1
+                    else:
+                        root = self.root_list.index(elem)
+                        short_hand = 0
 
-        a = 0
-        tmp = float(chord[a][1])
-        for i in range(num_beat - 1):
-            while beat[i] > tmp:
-                a += 1
-                tmp = float(chord[a][1])
-            if chord[a][2] == "C":
-                if chord[a][3] == 1:
-                    chord_vector[i][0] = 1
-                elif chord[a][3] == 0:
-                    chord_vector[i][1] = 1
-            elif chord[a][2] == "C#":
-                if chord[a][3] == 1:
-                    chord_vector[i][2] = 1
-                elif chord[a][3] == 0:
-                    chord_vector[i][3] = 1
-            elif chord[a][2] == "D":
-                if chord[a][3] == 1:
-                    chord_vector[i][4] = 1
-                elif chord[a][3] == 0:
-                    chord_vector[i][5] = 1
-            elif chord[a][2] == "D#":
-                if chord[a][3] == 1:
-                    chord_vector[i][6] = 1
-                elif chord[a][3] == 0:
-                    chord_vector[i][7] = 1
-            elif chord[a][2] == "E":
-                if chord[a][3] == 1:
-                    chord_vector[i][8] = 1
-                elif chord[a][3] == 0:
-                    chord_vector[i][9] = 1
-            elif chord[a][2] == "F":
-                if chord[a][3] == 1:
-                    chord_vector[i][10] = 1
-                elif chord[a][3] == 0:
-                    chord_vector[i][11] = 1
-            elif chord[a][2] == "F#":
-                if chord[a][3] == 1:
-                    chord_vector[i][12] = 1
-                elif chord[a][3] == 0:
-                    chord_vector[i][13] = 1
-            elif chord[a][2] == "G":
-                if chord[a][3] == 1:
-                    chord_vector[i][14] = 1
-                elif chord[a][3] == 0:
-                    chord_vector[i][15] = 1
-            elif chord[a][2] == "G#":
-                if chord[a][3] == 1:
-                    chord_vector[i][16] = 1
-                elif chord[a][3] == 0:
-                    chord_vector[i][17] = 1
-            elif chord[a][2] == "A":
-                if chord[a][3] == 1:
-                    chord_vector[i][18] = 1
-                elif chord[a][3] == 0:
-                    chord_vector[i][19] = 1
-            elif chord[a][2] == "A#":
-                if chord[a][3] == 1:
-                    chord_vector[i][20] = 1
-                elif chord[a][3] == 0:
-                    chord_vector[i][21] = 1
-            elif chord[a][2] == "B":
-                if chord[a][3] == 1:
-                    chord_vector[i][22] = 1
-                elif chord[a][3] == 0:
-                    chord_vector[i][23] = 1
-            else:
-                continue
+                if short_hand in [0, 3, 4, 6, 10, 12, 13, 15]:
+                    short_hand = 0
+                elif short_hand in [1, 2, 5, 7, 8, 9, 11, 14]:
+                    short_hand = 1
+                else:
+                    short_hand = -1
 
-        # pitch shift the labels of the chord
-        if pitch != 0:
-            chord_vector_shifted = np.zeros(chord_vector.shape)
-            chord_vector_shifted[:, (2 * pitch):] = chord_vector[:, :(-2 * pitch)]
-            chord_vector_shifted[:, :(2 * pitch)] = chord_vector[:, (-2 * pitch):]
-            chord_vector = chord_vector_shifted
+                chord.append([float(raw_array[i][0]), float(raw_array[i][1]), root, short_hand])
 
-        return chord_vector
+            # construct the chord_vector where each element of this vector is a one-hot code representing the chord of the beat
+            num_beat = np.shape(beat)[0]
+            chord_vector = np.zeros([num_beat - 1, self.encode_length])
+
+            a = 0  # a is index in chord array
+            tmp = float(chord[a][1])
+            for i in range(num_beat - 1):  # i is index as beat number
+                while beat[i] > tmp:
+                    a += 1
+                    tmp = float(chord[a][1])
+                root = chord[a][2]
+                short_hand = chord[a][3]
+
+                if root == -1 or short_hand == -1:
+                    chord_vector[i][-1] = 1
+                else:
+                    chord_vector[i][2 * root + short_hand] = 1
+
+            # pitch shift the labels of the chord
+            if pitch != 0:
+                chord_vector_currpitch = chord_vector[:, :self.encode_length - 1]
+                chord_vector_shifted = np.zeros(chord_vector_currpitch.shape)
+                chord_vector_shifted[:, (2 * pitch):] = chord_vector_currpitch[:, :(-2 * pitch)]
+                chord_vector_shifted[:, :(2 * pitch)] = chord_vector_currpitch[:, (-2 * pitch):]
+                chord_vector = np.concatenate((chord_vector_shifted, chord_vector[:, -1]), axis=-1)
+
+            return chord_vector
+        elif self.encode_length == 41:
+            raise NotImplementedError
+
+        return
+
+class SongReader:
+    def __init__(self, samples_per_beat, beats_per_window):
+        """
+        Initialize DataReader object
+        :param samples_per_beat: number of samples per beat
+        :param beats_per_window: number of beats per feature vector (window)
+
+        Construct a DataReader object to convert a WAV file to multiple feature vectors (one feature vector per beat)
+
+        """
+
+        self.spb = samples_per_beat
+        self.bpw = beats_per_window
+
+    def __call__(self, path, offset=None, bpm=None):
+        """
+        Convert a user input song to multiple feature vectors (one feature vector per beat)
+        :param path: path to the WAVE audio file
+        :param offset: offset of the song
+        :param bpm: bpm of the song
+
+        :return: ndarray of shape [num_beats - 1, self.spb * self.bpw, 2]
+
+        """
+
+        if offset is None:
+            raise ValueError('Requires offset value.')
+        if bpm is None:
+            raise ValueError('Requires bpm value.')
+
+        # read WAV file as ndarray
+        sr, raw_wav = wavfile.read(path)
+        length = raw_wav.shape[0]/sr
+
+        # Calculate the timing based on bpm and offset\
+        a = np.arange(length * bpm / 60)                # index of beats
+        beat = a * 60 / bpm + offset % (60 / bpm)       # timing of beats adjusted by offset
+        if beat[-1] > length:
+            beat = beat[:-1]
+
+        beat = np.int32(sr * beat)
+
+        num_beats = beat.shape[0]
+
+        # re-sample on each beat
+        feature_beat = np.zeros([num_beats - 1, self.spb, 2])
+        for i in range(num_beats - 1):
+            raw = raw_wav[beat[i]:beat[i + 1], :]
+            resampled = samplerate.resample(raw, self.spb/raw.shape[0])
+            feature_beat[i, :resampled.shape[0], :] = resampled
+
+        # construct feature windows from feature_beat
+        feature_window = np.zeros([num_beats - 1, self.spb * self.bpw, 2])
+        for i in range(num_beats - 1):
+            for j in range(self.bpw):
+                if j == 0:
+                    feature_window[i, ((j - 1) * self.spb):, :] = feature_beat[i]
+                elif i >= j:
+                    feature_window[i, ((j - 1) * self.spb):(j * self.spb), :] = feature_beat[i - j]
+
+        return feature_window
