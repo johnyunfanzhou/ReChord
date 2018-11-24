58c13fff3562defde9da033ab3cd6508d187c728
Sophie <zouyining@live.cn>
2018-11-24 16:03:51 -0500

    data balancing version.2

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import torch
 from torch.utils.data import DataLoader
 from dataset import ReChordDataset
 from model import CNNReChord
+import random
 
 
 samples_per_beat = 3072
 beats_per_window = 9
-encode_length = 25 -1
+encode_length = 25
 data = [('01', '01'), ('01', '02'), ('01', '03'), ('01', '04'), ('01', '05'), ('01', '06'), ('01', '07'),
         ('01', '08'), ('01', '09'), ('01', '10'), ('01', '11'), ('01', '12'), ('01', '13'), ('01', '14'),
         ('02', '01'), ('02', '02'), ('02', '03'), ('02', '04'), ('02', '06'), ('02', '07'), ('02', '08'),
@@ -21,6 +22,110 @@ data = [('01', '01'), ('01', '02'), ('01', '03'), ('01', '04'), ('01', '05'), ('
         ('07', '08'), ('07', '09'), ('07', '10'), ('07', '11'), ('07', '12'), ('07', '13'), ('07', '14'),
         ('12', '01'), ('12', '02'), ('12', '03'), ('12', '04'), ('12', '05'), ('12', '06'), ('12', '07'),
         ('12', '08'), ('12', '09'), ('12', '10'), ('12', '11'), ('12', '12')]
+
+
+def load_data_new(batch_size, i):
+    features = np.empty([0, samples_per_beat * beats_per_window, 2])
+    labels = np.empty([0, encode_length])
+    features_0 = np.empty([0, samples_per_beat * beats_per_window, 2])
+    labels_0 = np.empty([0, encode_length])
+    chord_dic = {}
+    chord_dic['major'] = 0
+    chord_dic['minor'] = 0
+    major_ind = []
+    minor_ind = []
+    chosen_ind = []
+    t = 0
+    for album, song in data:
+        feature_file = './data/features/{}_{}_{}.npy'.format(album, song, '0')
+        label_file = './data/labels/{}_{}_{}.npy'.format(album, song, '0')
+        tmp_feature = np.load(feature_file)
+        ind = tmp_feature.shape[0]
+        tmp_feature = tmp_feature[0+int(i/60*ind):int((1+i)/60*ind)]
+        tmp_label = np.load(label_file)
+        ind = tmp_label.shape[0]
+        tmp_label = tmp_label[0+int(i/60*ind):int((1+i)/60*ind)]
+        features_0 = np.concatenate((features_0, tmp_feature), axis=0)
+        labels_0 = np.concatenate((labels_0, tmp_label), axis=0)
+        i += 1
+        if i > 60:
+            i = i % 60
+
+    # counting the number of major and minor chords and storing their index
+    for j in range(labels_0.shape[0]):
+        a = np.argmax(labels_0[j], axis=0)
+        if (a % 2 == 0) and (a < 24):
+            chord_dic['major'] += 1
+            major_ind.append(j)
+        elif a % 2 == 1:
+            chord_dic['minor'] += 1
+            minor_ind.append(j)
+
+    # balancing the data
+    if (chord_dic['major'] != 0) and (chord_dic['minor'] != 0):
+        if chord_dic['minor'] < chord_dic['major']:
+            x = random.sample(range(len(major_ind)), chord_dic['minor'])
+            y = random.sample(range(len(minor_ind)), chord_dic['minor'])
+            while t < chord_dic['minor']:
+                b = major_ind[x[t]]
+                features = np.concatenate((features, features_0[b].reshape(1, features_0.shape[1], features_0.shape[2])), axis=0)
+                labels = np.concatenate((labels, labels_0[b].reshape(1, labels.shape[1])), axis=0)
+                chosen_ind.append(b)
+
+                b = minor_ind[y[t]]
+                features = np.concatenate((features, features_0[b].reshape(1, features_0.shape[1], features_0.shape[2])), axis=0)
+                labels = np.concatenate((labels, labels_0[b].reshape(1, labels.shape[1])), axis=0)
+                chosen_ind.append(b)
+                t += 1
+        elif chord_dic['minor'] > chord_dic['major']:
+            t = 0
+            x = random.sample(range(len(major_ind)), chord_dic['major'])
+            y = random.sample(range(len(minor_ind)), chord_dic['major'])
+            while t < chord_dic['major']:
+                b = major_ind[x[t]]
+                features = np.concatenate((features, features_0[b].reshape(1, features_0.shape[1], features_0.shape[2])), axis=0)
+                labels = np.concatenate((labels, labels_0[b].reshape(1, labels.shape[1])), axis=0)
+                chosen_ind.append(b)
+
+                b = minor_ind[y[t]]
+                features = np.concatenate((features, features_0[b].reshape(1, features_0.shape[1], features_0.shape[2])), axis=0)
+                labels = np.concatenate((labels, labels_0[b].reshape(1, labels.shape[1])), axis=0)
+                chosen_ind.append(b)
+                t += 1
+        else:
+            features = np.concatenate((features, features_0), axis=0)
+            labels = np.concatenate((labels, labels_0), axis=0)
+
+    # loading other pitches
+    for pitch in ['-5', '-4', '-3', '-2', '-1', '1', '2', '3', '4', '5', '6']:
+        i = 0
+        for album, song in data:
+            feature_file = './data/features/{}_{}_{}.npy'.format(album, song, pitch)
+            label_file = './data/labels/{}_{}_{}.npy'.format(album, song, pitch)
+            tmp_feature = np.load(feature_file)
+            w = tmp_feature.shape[0]
+            tmp_feature = tmp_feature[0 + int(i / 60 * w):int((1 + i) / 60 * w)]
+            tmp_label = np.load(label_file)
+            w = tmp_label.shape[0]
+            tmp_label = tmp_label[0 + int(i / 60 * w):int((1 + i) / 60 * w)]
+            features_0 = np.concatenate((features_0, tmp_feature), axis=0)
+            labels_0 = np.concatenate((labels_0, tmp_label), axis=0)
+            i += 1
+            if i > 60:
+                i = i % 60
+        for ind in chosen_ind:
+            features = np.concatenate((features, features_0[ind].reshape(1, features.shape[1], features.shape[2])), axis=0)
+            labels = np.concatenate((labels, labels_0[ind].reshape(1, labels.shape[1])), axis=0)
+
+    train_data, val_data, train_label, val_label = train_test_split(features, labels, test_size=0.2)
+
+    train_dataset = ReChordDataset(train_data, train_label)
+    val_dataset = ReChordDataset(val_data, val_label)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, val_loader
 
 
 def load_data(batch_size, album, song):
@@ -56,8 +161,6 @@ def load_data(batch_size, album, song):
         arg_feature = np.empty([0, samples_per_beat * beats_per_window, 2])
         arg_label = np.empty([0, encode_length])
         count = 0
-        m = 0
-        n = 0
 
         if chord_dic['major'] > chord_dic['minor']:
             for m in range(tmp_label.shape[0]):
@@ -76,7 +179,6 @@ def load_data(batch_size, album, song):
                                 axis=0)
                             arg_label = np.concatenate((arg_label, tmp_label[m].reshape(1, tmp_label.shape[1])), axis=0)
                         break
-                m += 1
         elif chord_dic['major'] < chord_dic['minor']:
             count = 0
             for n in range(tmp_label.shape[0]):
@@ -99,13 +201,9 @@ def load_data(batch_size, album, song):
                                 axis=0)
                             arg_label = np.concatenate((arg_label, tmp_label[m].reshape(1, tmp_label.shape[1])), axis=0)
                         break
-                n += 1
 
         features = np.concatenate((features, arg_feature), axis=0)
         labels = np.concatenate((labels, arg_label), axis=0)
-
-
-
 
     train_data, val_data, train_label, val_label = train_test_split(features, labels, test_size=0.2)
 
@@ -146,35 +244,6 @@ def evaluate(model, val_loader):
     return float(total_correct) / len(val_loader.dataset)
 
 
-def data_balance():
-    chord_dic = {}
-    chord_dic['major'] = 0
-    chord_dic['minor'] = 0
-
-    features = np.empty([0, samples_per_beat * beats_per_window, 2])
-    labels = np.empty([0, encode_length - 1])
-    # album = '01'
-    # song = '01'
-    for album, song in data:
-        feature_file = './data/features/{}_{}_{}.npy'.format(album, song, '0')
-        label_file = './data/labels/{}_{}_{}.npy'.format(album, song, '0')
-        features = np.concatenate((features, np.load(feature_file)), axis=0)
-        labels = np.concatenate((labels, np.load(label_file)), axis=0)
-
-    dataset = ReChordDataset(features, labels)
-
-    for i in range(dataset.__len__()):
-        for j in range(24):
-            if dataset.y[i][j] == 1.0:
-                if j % 2 == 0:
-                    chord_dic['major'] += 1
-                else:
-                    chord_dic['minor'] += 1
-                break
-
-    return chord_dic
-
-
 def main(batch_size, lr, epochs, config=0, pre_trained=-1):
     model, loss_fnc, optimizer = load_model(lr, config=config, pre_trained=pre_trained)
 
@@ -184,10 +253,8 @@ def main(batch_size, lr, epochs, config=0, pre_trained=-1):
         train_accuracy = []
         val_accuracy = []
 
-        for album, song in data:
-            print('\tAlbum {}, song {} ...'.format(album, song))
-
-            train_loader, val_loader = load_data(batch_size, album, song)
+        for k in range(60):
+            train_loader, val_loader = load_data_new(batch_size, k)
 
             for i, batch in enumerate(train_loader):
                 # get the inputs
@@ -210,8 +277,8 @@ def main(batch_size, lr, epochs, config=0, pre_trained=-1):
             train_accuracy.append(train_acc)
             val_accuracy.append(val_acc)
 
-            print('\tFinished album {} song {} | Training accuracy: {} | Validation accuracy: {}'.format(
-                album, song, train_acc, val_acc
+            print('\tFinished batch {} | Training accuracy: {} | Validation accuracy: {}'.format(
+                k, train_acc, val_acc
             ))
 
         average_train_acc = sum(train_accuracy)/len(train_accuracy)
@@ -230,4 +297,5 @@ def main(batch_size, lr, epochs, config=0, pre_trained=-1):
 
 
 if __name__ == "__main__":
+    # load_data_new(64, 0)
     main(64, 0.001, 50, config=0, pre_trained=-1)
